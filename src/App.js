bf0f8f4a56bd0397e4c2b3e1063afcf2fb3802c7
darektoa <darektoa@gmail.com>
2021-01-12 09:06:46 +0700

    First Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import logo from './images/logo-gmail.png';
+import hero from './images/hero.png';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Hero />
+    </main>
   );
 }
 
-export default App;
+const Header = () => {
+  return (
+    <header>
+      <nav className="container">
+        <figure>
+          <img src={logo} alt="logo" />
+          <figcaption>Gmail</figcaption>
+        </figure>
+        <ul>
+          <li><a href="/">Untuk Bisnis</a></li>
+          <li><a href="/">Login</a></li>
+          <li><a href="/" className="register">Buat Akun</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const Hero = () => {
+  return (
+    <section id="hero" className="container">
+      <div className="text-box">
+        <h1>Selesaikan lebih banyak hal dengan Gmail</h1>
+        <p>Solusi untuk rapat video dengan fitur teks otomatis dan berbagi layar hingga maksimum 100 orang. Gabung sekarang dengan Google Meet di Gmail.</p>
+        <a href="/" className="register">Buat Akun</a>
+      </div>
+
+      <div className="img-box">
+        <img src={hero} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export {
+  App,
+  Header,
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './font-size.css';
+import { App, Header } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
